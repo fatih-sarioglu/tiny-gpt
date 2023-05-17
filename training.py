f8e159dfed50b4a271e4edc6aa2f4da8692c972a
Fatih <ftihsrglu@gmail.com>
2023-05-17 10:11:40 +0000

    may 17

--- training.py
+++ training.py
@@ -7,6 +7,7 @@ block_size = 8
 max_iters = 3000
 eval_interval = 300
 learning_rate = 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 
 torch.manual_seed(31)
@@ -43,8 +44,80 @@ def get_batch(data, batch_size=4, block_size=8): # 4 different chunks with conte
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    
-    x, y = 
+    x, y = x.to(device), y.to(device)
 
     return x, y
 
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out['split'] = loss.mean()
+    model.train()
+    return out
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token reads the logit for the next token
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        
+    def forward(self, idx, targets=None):
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+        
+        if targets is None: # to successfully run the generate func
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = functional.cross_entropy(logits, targets)
+        
+        return logits, loss
+    
+    def generate(self, idx, max_tokens):
+        # idx is (B,T) array of indices
+        for _ in range(max_tokens):
+            # getting the predictions
+            logits, loss = self(idx)
+            # focusing on the last indices
+            logits = logits[:, -1, :] # becomes (B,C)
+            # calculating the probabilities
+            probs = functional.softmax(logits, dim=1) # (B,C)
+            # sampling from prob distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B,1)
+            # append the sampled index to the end of the context
+            idx = torch.cat((idx, idx_next), dim=1) # becomes (B,T+1)
+        return idx
+
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
+
+# create optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss: {losses['train']:.4f}, val loss: {losses['val']:.4f}")
+
+    xb, yb = get_batch('train')
+
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+context = torch.zeros((1,1), dtype=torch.long, device=device)
+print(decode(m.generate(context), max_tokens=100)[0].tolist())
